9671610b3bf0f136460f83d607b16199d201610b
vicheaaaa <sengvichea11th@gmail.com>
2024-12-19 10:50:47 +0700

    z-indec

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,7 +49,7 @@ const Navbar = () => {
                 <img onClick={()=> token ? null : navigate('/login') } className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
                 {/* Dropdown Menu */}
                 {token && 
-                <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4 z-50'>
                     <div className='flex flex-col gap-2 w-36 py-3 px-5  bg-slate-100 text-gray-500 rounded'>
                         <p className='cursor-pointer hover:text-black'>My Profile</p>
                         <p onClick={()=>navigate('/orders')} className='cursor-pointer hover:text-black'>Orders</p>
