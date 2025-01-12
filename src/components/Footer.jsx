e5e7ab9f711379180dd57a70739b2dd7d1f57c98
vicheaaaa <123868063+vicheaaaa@users.noreply.github.com>
2025-01-12 19:41:23 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -26,8 +26,8 @@ const Footer = () => {
         <div>
             <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
             <ul className='flex flex-col gap-1 text-gray-600'>
-                <li>+1-212-456-7890</li>
-                <li>contact@foreveryou.com</li>
+                <li>+85592632602</li>
+                <li>contact@e10shop.com</li>
             </ul>
         </div>
 
