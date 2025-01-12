f8cd9e199a2969735d0320fc28ec9672e8c06188
vicheaaaa <123868063+vicheaaaa@users.noreply.github.com>
2025-01-12 19:47:31 +0700

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
           <p className=' text-gray-500'>Snooker Fun<br /> Phnom Penh st271, Phnom Penh, Cambodia</p>
-          <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: admin@e10shop.com</p>
+          <p className=' text-gray-500'>Tel: (855) 926-32603 <br /> Email: admin@e10shop.com</p>
           <p className='font-semibold text-xl text-gray-600'>Careers at E10Shop</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
